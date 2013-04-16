9249ecdc9cf5e3dea9ed303c5d725e1d5787a062
Rosen <rocky_2005@abv.bg>
2013-04-16 22:33:29 +0300

    commit

---
+++ high-quality-code/5. Using Variables, Data, Expressions and Constants/VariableNamingSimplifiedExpressions.cs
@@ -0,0 +1,24 @@
+public class Size
+{
+  public double Width; 
+  public double Height;
+  
+  public Size(double width, double height)
+  {
+    this.Width = width;
+    this.Height = height;
+  }
+}
+
+public static Size GetRotatedSize(Size oldSize, double angleOfTheFigureRotated)
+{
+	double angleCos = Math.Cos(angleOfTheFigureRotated);
+	double angleCosAbs = Math.Abs(angleCos);
+	double angleSin = Math.Sin(angleOfTheFigureRotated);
+	double angleSinAbs = Math.Sin(angleSin);
+	double width = angleCosAbs * oldSize.width + angleSinAbs * oldSize.height;
+	double height = angleSinAbs * oldSize.width + angleCosAbs * oldSize.height;
+	Size rotatedSize = new Size(width, height);
+	
+	return rotatedSize;
+}
---
+++ high-quality-code/5. Using Variables, Data, Expressions and Constants/VariableUsage.cs
@@ -0,0 +1,31 @@
+public void PrintStatistics(double[] data, int count)
+{
+    double max = data[0];
+    for (int i = 0; i < count; i++)
+    {
+        if (data[i] > max)
+        {
+            max = data[i];
+        }
+    }
+    PrintMax(max);
+	
+    double min = data[0];
+    for (int i = 0; i < count; i++)
+    {
+        if (data[i] < min)
+        {
+            min = data[i];
+        }
+    }
+    PrintMin(min);
+
+    double sum = 0;
+    for (int i = 0; i < count; i++)
+    {
+        sum += data[i];
+    }
+	
+	double average = sum / count;
+    PrintAvg(average);
+}
