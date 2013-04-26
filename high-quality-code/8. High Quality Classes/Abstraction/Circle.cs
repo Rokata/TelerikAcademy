6fcb0f84bcb7c3397cf3559a8355fa4836ea060e
Rosen <rocky_2005@abv.bg>
2013-04-26 14:58:52 +0300

    commit

---
+++ high-quality-code/8. High Quality Classes/Abstraction/Circle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Abstraction
+{
+    class Circle : Figure
+    {
+        private double radius;
+
+        public Circle() : base()
+        {
+            this.Radius = 1;
+        }
+
+        public Circle(double radius)
+            : base()
+        {
+            this.Radius = radius;
+        }
+
+        public double Radius
+        {
+            get { return radius; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Radius must be positive value.");
+
+                this.radius = value;
+            }
+        }
+
+        public override double CalcPerimeter()
+        {
+            double perimeter = 2 * Math.PI * this.Radius;
+            return perimeter;
+        }
+
+        public override double CalcSurface()
+        {
+            double surface = Math.PI * this.Radius * this.Radius;
+            return surface;
+        }
+    }
+}
