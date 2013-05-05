36e0d46e7b2a7732b3bd6bf6086524867ae4a7d9
Rosen <rocky_2005@abv.bg>
2013-05-06 00:10:42 +0300

    new

--- high-quality-code/11. Unit Testing/SchoolLib/Properties/AssemblyInfo.cs
+++ high-quality-code/11. Unit Testing/SchoolLib/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("School")]
+[assembly: AssemblyTitle("SchoolLib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("School")]
+[assembly: AssemblyProduct("SchoolLib")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a3f88d92-cb06-4325-8b3a-2110ccb69f28")]
+[assembly: Guid("c7428123-71c4-408f-8208-74a446012eea")]
 
 // Version information for an assembly consists of the following four values:
 //
