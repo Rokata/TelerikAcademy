11942e032dbe3d263c61c5bc7f6c4097dffb063e
Rosen <rocky_2005@abv.bg>
2013-05-12 11:24:03 +0300

    new

---
+++ js-part2/5. JavaScript Event Model/5. JavaScript Event Model/scripts/carousel.js
@@ -0,0 +1,32 @@
+﻿window.onload = function () {
+    var imgSources = ["images/img1.jpg", "images/img2.jpg", "images/img3.jpg"];
+    var prev = document.getElementById("arrow-back");
+    var next = document.getElementById("arrow-next");
+    var current = document.getElementById("current");
+    var currentIndex = 0;
+
+    prev.onclick = prevOnClick;
+    next.onclick = nextOnClick;
+
+    function prevOnClick() {
+        if (currentIndex == 0) {
+            currentIndex = imgSources.length - 1;
+        }
+        else {
+            currentIndex--;
+        }
+
+        current.setAttribute('src', imgSources[currentIndex]);
+    }
+
+    function nextOnClick() {
+        if (currentIndex == imgSources.length - 1) {
+            currentIndex = 0;
+        }
+        else {
+            currentIndex++;
+        }
+
+        current.setAttribute('src', imgSources[currentIndex]);
+    }
+}
---
+++ js-part2/5. JavaScript Event Model/5. JavaScript Event Model/scripts/todo.js
@@ -0,0 +1,52 @@
+﻿var todoList = document.getElementById("todo");
+
+function addItem() {
+    var value = document.getElementById("addItem").value;
+    var item = document.createElement("li");
+    item.innerHTML = value;
+    item.onclick = markItem;
+    todoList.appendChild(item);
+}
+
+function markItem(ev) {
+    if (!ev) ev = window.event;
+
+    var target = ev.srcElement;
+
+    if (target.style.color == "red")
+        target.style.color = "black";
+    else {
+        target.style.color = "red";
+    }
+}
+
+function removeItems() {
+    for (var i = 0; i < todoList.childNodes.length; i++) {
+        var current = todoList.childNodes[i];
+
+        if (current.style.color == "red") {
+            todoList.removeChild(current);
+            i--; // the list resizes after removal
+        }
+    }
+}
+
+function hideSelected() {
+    for (var i = 0; i < todoList.childNodes.length; i++) {
+        var current = todoList.childNodes[i];
+
+        if (current.style.color == "red") {
+            current.style.color = "black";
+            current.style.display = "none";
+        }
+    }
+}
+
+function show() {
+    for (var i = 0; i < todoList.childNodes.length; i++) {
+        var current = todoList.childNodes[i];
+
+        if (current.style.display == "none")
+            current.style.display = "block";
+    }
+}
